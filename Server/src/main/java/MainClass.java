62521effc4e1ea345a9980c867e357bdaa53745f
Cristina Jipescu <cristina.cristina2109@gmail.com>
2021-05-31 11:50:18 +0300

    Implement MainClass in Server

--- Server/src/main/java/MainClass.java
+++ Server/src/main/java/MainClass.java
@@ -1,6 +1,22 @@
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import services.ChatMessageService;
+
+import java.io.IOException;
+
 public class MainClass {
-
     public static void main(String[] args) {
+        try {
+            Server server = ServerBuilder
+                    .forPort(1337)
+                    .addService(new ChatMessageService())
+                    .build();
 
+            server.start();
+
+            server.awaitTermination();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
