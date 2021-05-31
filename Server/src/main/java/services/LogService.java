6aa7484b16af232008c4ec4d9dc0a52a72050d7a
Cristina Jipescu <cristina.cristina2109@gmail.com>
2021-05-31 11:37:35 +0300

    Implement onCompleted method in LogService

--- Server/src/main/java/services/LogService.java
+++ Server/src/main/java/services/LogService.java
@@ -52,7 +52,9 @@ public class LogService extends LogServiceGrpc.LogServiceImplBase {
 
             @Override
             public void onCompleted() {
-
+                synchronized (observers) {
+                    observers.remove(responseObserver);
+                }
             }
         };
     }
